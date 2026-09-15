ebd28a71ddef381ebc4813fd721df175544144da
karthik-SP22 <spkarthik886@gmail.com>
2026-09-15 18:42:57 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        a=sorted(nums)
+        for i in range(len(a)-1):
+            if a[i]==a[i+1]:
+                return True
+                
+            
+
+        return False
+
+
+        
